fe124b12c2161b28cfac42a083a157e91c529e13
wang1993 <374083765@qq.com>
2017-06-21 09:59:47 +0800

    untitled1

--- untitled1/main.cpp
+++ untitled1/main.cpp
@@ -1,10 +1,13 @@
-#include <iostream>
+﻿#include <iostream>
 
 using namespace std;
 
 int main()
 {
-    cout << "Hello World!" << endl;
+    for( int i=0; i<10; i++ )
+    {
+        cout<<i<<endl;
+    }
     return 0;
 }
 
